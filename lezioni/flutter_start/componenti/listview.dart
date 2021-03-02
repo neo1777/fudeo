f19d9ccbc5b8028e6c97079c1b4e39fbe7c8fdb4
neo1777 <banner@nicolainformatica.it>
2021-03-02 16:31:26 +0100

    less 2 list view

--- lezioni/flutter_start/componenti/listview.dart
+++ lezioni/flutter_start/componenti/listview.dart
@@ -1,5 +1,34 @@
 import 'package:flutter/material.dart';
 
+class Day {
+  final int nr;
+  String task;
+  Day(this.nr);
+}
+
+class Month {
+  final String name;
+  List<Day> days;
+
+  Month(this.name, int daysNr) {
+    days = List.generate(daysNr, (dayNr) => Day(dayNr + 1));
+  }
+}
+
+final months = [
+  Month('Gennaio', 31),
+  Month('Febbraio', 28),
+  Month('Marzo', 31),
+  Month('Aprile', 30),
+  Month('Maggio', 31),
+  Month('Giugno', 30),
+  Month('Luglio', 31),
+  Month('Agosto', 31),
+  Month('Settembre', 30),
+  Month('Ottobre', 31),
+  Month('Novembre', 30),
+  Month('Dicembre', 31),
+];
 void main() {
   runApp(App1777());
 }
@@ -9,7 +38,19 @@ class App1777 extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //home: HomePage(),
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
